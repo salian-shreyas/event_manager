794b5f490bb118543ed6f9bdf8e39083292fafc4
Shreyas G Salian <69745409+salian-shreyas@users.noreply.github.com>
2024-12-29 11:04:13 +0530

    Show government representatives

---
+++ lib/event_manager.rb
@@ -0,0 +1,36 @@
+require 'csv'
+require 'google-apis-civicinfo_v2'
+
+civic_info = Google::Apis::CivicinfoV2::CivicInfoService.new
+civic_info.key = File.read('secret.key').strip
+
+def clean_zipcode(zipcode)
+  zipcode.to_s.rjust(5, '0')[0..4]
+end
+
+puts "Event manager initialized!"
+
+contents = CSV.open(
+  'event_attendees.csv',
+  headers: true,
+  header_converters: :symbol
+)
+
+contents.each do |row|
+  name = row[:first_name]
+
+  zipcode = clean_zipcode row[:zipcode]
+
+  begin
+    legislators = civic_info.representative_info_by_address(
+      address: zipcode,
+      levels: 'country',
+      roles: ['legislatorUpperBody', 'legislatorLowerBody']
+    )
+    legislators = legislators.officials
+  rescue
+    "You can find your representatives by visiting www.commoncause.org/take-action/find-elected-officials"
+  end
+
+  puts "#{name} #{zipcode}\n\n#{legislators}\n\n"
+end
