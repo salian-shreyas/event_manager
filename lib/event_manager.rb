aee3029a8377f8389f3143bcd2c960d5cc1fd30f
Shreyas G Salian <69745409+salian-shreyas@users.noreply.github.com>
2024-12-29 13:41:31 +0530

    Display form letter

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -34,13 +34,17 @@ contents = CSV.open(
   header_converters: :symbol
 )
 
+template_letter = File.read('form_letter.html')
+
 contents.each do |row|
   name = row[:first_name]
 
   zipcode = clean_zipcode row[:zipcode]
 
   legislators = legislators_by_zipcode zipcode
-  
 
-  puts "#{name} #{zipcode} #{legislators}\n"
+  personal_letter = template_letter.gsub('FIRST_NAME', name)
+  personal_letter.gsub!('LEGISLATORS', legislators)
+
+  puts personal_letter
 end
